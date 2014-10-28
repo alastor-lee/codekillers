0b5c28fe5554690c0137bf08d9cc2f1e9e0bbe3f
cmckenzie6 <cmckenzie6@student.gsu.edu>
2014-10-28 16:56:36 -0400

    Run HotelManagementSystem.java to start program.

---
+++ Engine/HotelManagementSystem.java
@@ -0,0 +1,13 @@
+package Engine;
+
+/**
+ * This is the main class for the program. This is the file that you will compile from the command line.
+ */
+public class HotelManagementSystem {
+    
+    public static void main(String[] args){
+        MainGUI.LoginFrame gui = new MainGUI.LoginFrame();
+        gui.setVisible(true);
+    }
+    
+}
