fd905f4271d37199306c1a3b747406c33bb8a0bf
cmckenzie6 <cmckenzie6@student.gsu.edu>
2014-11-30 18:26:33 -0500

    Added simple SMTP mail class

---
+++ Engine/Mail.java
@@ -0,0 +1,44 @@
+package Engine;
+
+import java.net.*;
+import java.io.*;
+
+/*
+ * Super simple email class that sends an email using SMTP. Currently I'm using the GSU mail server, not sure if that is okay... haha
+ */
+public class Mail {
+
+    public void sendMail(String guestEmail, String message) {
+        try {
+            Socket GSUmail = new Socket("mail.gsu.edu", 25);
+            outStream = new PrintWriter(GSUmail.getOutputStream());
+            inStream = new BufferedReader(new InputStreamReader(GSUmail.getInputStream()));
+            String host = InetAddress.getLocalHost().getHostName();
+            send("HELO " + host);
+            send("MAIL FROM: " + "hotelmanagementsystem@codekillers.com");
+            send("RCPT TO: " + guestEmail);
+            send("DATA\n"+message+"\n.");
+            send("QUIT");
+            GSUmail.close();
+            outStream.close();
+            inStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}
+
+    public void send(String s) throws IOException {
+        if (s != null) {
+            outStream.println(s);
+            outStream.flush();
+        }
+        String line;
+        if ((line = inStream.readLine()) != null)
+        {
+            System.out.println(line);
+        }
+    }
+
+    private PrintWriter outStream;
+    private BufferedReader inStream;
+}
