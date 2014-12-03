9b763e07c263094e58d4056eacb14061a9c08852
cmckenzie6 <cmckenzie6@student.gsu.edu>
2014-12-02 20:59:01 -0500

    Changed "Login" to "Log In"

--- mainframe/gui/LoginFrame.java
+++ mainframe/gui/LoginFrame.java
@@ -45,7 +45,7 @@ public class LoginFrame extends javax.swing.JFrame {
 
         PasswordLabel.setText("Password:");
 
-        LoginButton.setText("Login");
+        LoginButton.setText("Log In");
         LoginButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 LoginButtonActionPerformed(evt);
